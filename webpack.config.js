a62f100558e66c46aa5dd5b8c6df1e87f2f7f57a
masato yoshioka <yoshioka0627@gmail.com>
2016-08-09 18:02:05 +0900

    change webpack becouse bulma-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
-    entry: [
-      "./src/js/index.js",
-    ],
+    entry: "./src/js/index.js",
     output: {
         path: __dirname + "/public/dist/",
         filename: "bundle.js"
@@ -9,6 +9,13 @@ module.exports = {
     module: {
         loaders: [
           { test: /\.js$/,   loader: "babel-loader", exclude: /node_modules/ },
+          { test: /\.sass$/, 
+            loaders: ["style", "css?modules&importLoaders=2", "sass", "bulma?theme=node_modules/bulma/bulma.sass"]
+          }
         ]
-    }
-};
+    },
+    resolve: {
+      extensions: ["", ".webpack.js", ".web.js", ".js", ".sass"]
+    },
+    devtool: "source-map",
+}
