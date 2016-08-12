831c3f6b62b7f7b75ba0914103387d161cab0f5f
masato yoshioka <yoshioka0627@gmail.com>
2016-08-12 10:01:31 +0900

    font awesome

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ module.exports = {
     entry: "./src/js/index.js",
     output: {
         path: __dirname + "/public" ,
-        filename: "bundle.js"
+        filename: "/dist/bundle.js"
     },
     module: {
         loaders: [
           { test: /\.js$/,   loader: "babel-loader", exclude: /node_modules/ },
           { test: /\.sass$/, loader: ExtractTextPlugin.extract("style", ["css?modules&importLoaders=2", "sass", "bulma?theme=node_modules/bulma/bulma.sass"])},
-          { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
-          { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+          { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff&name=./fonts/[hash].[ext]" },
+          { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader?name=./fonts/[hash].[ext]" }
         ]
     },
     resolve: {
@@ -19,6 +19,6 @@ module.exports = {
     },
     devtool: "source-map",
     plugins: [
-      new ExtractTextPlugin('bundle.css')
+      new ExtractTextPlugin('./dist/bundle.css')
     ]
 }
